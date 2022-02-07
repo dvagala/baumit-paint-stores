77c99af0a7db3ac8c03c10ed1a364e7d50277694
Dominik Vagala <37017489+dvagala@users.noreply.github.com>
2022-02-07 15:30:14 +0100

    Update paint_stores.dart

--- paint_stores.dart
+++ paint_stores.dart
@@ -1,6 +1,7 @@
 const paintStoresIonitPainterSk = [
   
-  
+  // Inštrukcie: pri pridávaní novej predaje iba nakopíruj tento jeden PaintStore blok, a vymeň údaje
+  // Lattitude a Longitude zistíš tak, že to googlu si napíšeš adresu, klikneš na Google Maps odkaz, čo ti tam hodí, a v tom linku to budeš mať zakódované. Napr: https://www.google.com/maps/place/Nová+7,+917+01+Trnava/@48.364157,17.6093464,17z/data=!3m1!4b1!4m5!3m4!1s0x476b5f98974f2f77:0xc94e801bd7ab9aaa!8m2!3d48.364157!4d17.6115351
   PaintStore(
     name: 'Stavebniny DEK s.r.o.',
     latLng: LatLng(48.3649014, 17.6123664),
