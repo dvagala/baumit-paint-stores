a58b38fbe766ff095c52326708da7805a7988aed
igofree <99345519+igofree@users.noreply.github.com>
2022-02-09 18:03:15 +0100

    Update paint_stores.dart

--- paint_stores.dart
+++ paint_stores.dart
@@ -28,5 +28,561 @@ const paintStoresIonitPainterSk = [
   
   
   
+  PaintStore(
+    name: 'WOODCOTE GROUP s.r.o.',
+    latLng: LatLng( 48.57783626859324, 18.87087695519509),
+    address: 'Priemyselná 2',
+    zipCode: '965 01',
+    town: 'Žiar nad Hronom',
+    phoneNumber: '0903/731998',
+    email: 'ziar@woodcote.sk',
+    website: 'www.woodcote.sk',
+  ),
   
-];
+  
+  
+    PaintStore(
+    name: 'AXA-PROPERTY s.r.o.',
+    latLng: LatLng( 48.952888969305214, 20.558947439869574 ),
+    address: 'Radlinského 17',
+    zipCode: '052 01',
+    town: 'Spišská Nová Ves',
+    phoneNumber: '0915/856453',
+    email: 'info@axaproperty.sk',
+    website: 'axaproperty.sk',
+  ),
+  
+  
+   
+    PaintStore(
+    name: 'STAVMAT STAVEBNINY, s.r.o.',
+    latLng: LatLng( 49.05508911418738, 18.91196359984228 ),
+    address: 'Na Bystričku 14',
+    zipCode: '036 01',
+    town: 'Martin',
+    phoneNumber: '0905/664887',
+    email: 'martin@stavmat.sk',
+    website: 'www.stavmat.sk',
+  ),
+  
+  
+  
+    PaintStore(
+    name: 'Stavebniny DEK s.r.o.',
+    latLng: LatLng( 49.20475947932288, 18.728198939881928 ),
+    address: 'Kamenná ul. 6',
+    zipCode: '010 01',
+    town: 'Žilina',
+    phoneNumber: '02/32 22 30 22',
+    email: 'zilina@dek.sk',
+    website: 'dek.sk',
+  ),
+  
+  
+  
+    PaintStore(
+    name: 'MPL TRADING spol. s r.o.',
+    latLng: LatLng( 49.20974497026296, 18.724934697554062 ),
+    address: 'Bánovská cesta 5',
+    zipCode: '010 01',
+    town: 'Žilina',
+    phoneNumber: '041/7234281',
+    email: 'za@mpl.sk',
+    website: 'www.mpl.sk',
+  ),
+  
+  
+  
+    PaintStore(
+    name: 'STAVMAT STAVEBNINY, s.r.o.',
+    latLng: LatLng( 49.20980805177126, 18.724945426390004 ),
+    address: 'Bánovská cesta 5',
+    zipCode: '010 01',
+    town: 'Žilina',
+    phoneNumber: '0903/555107',
+    email: 'zilina@stavmat.sk',
+    website: 'www.stavmat.sk',
+  ),
+  
+  
+  
+    PaintStore(
+    name: 'BK DOM s.r.o.',
+    latLng: LatLng( 48.21393542840205, 18.585520099357247 ),
+    address: 'Mestský majer 2',
+    zipCode: '934 01',
+    town: 'Levice',
+    phoneNumber: '036/6334932',
+    email: 'bkdom@bkdom.sk',
+    website: 'www.bkdom.sk',
+  ),
+  
+  
+    
+    PaintStore(
+    name: 'GARBIAR Stavebniny s.r.o. (člen SOAS)',
+    latLng: LatLng( 49.27834417866189, 19.602054897557416 ),
+    address: 'Pri moste',
+    zipCode: '027 32',
+    town: 'Habovka',
+    phoneNumber: '043/5395898',
+    email: 'habovka@garbiar.sk',
+    website: 'www.garbiar.sk',
+  ),
+  
+  
+    
+    PaintStore(
+    name: 'STAVMAT STAVEBNINY, s.r.o.',
+    latLng: LatLng( 48.69111619697275, 21.258407193825523 ),
+    address: 'Rastislavova 2382/91',
+    zipCode: '040 01',
+    town: 'Košice-Juh',
+    phoneNumber: '0903/290807',
+    email: 'ke.predajna@stavmat.sk',
+    website: 'www.stavmat.sk',
+  ),
+  
+    
+  
+    PaintStore(
+    name: 'STAVMAT STAVEBNINY, s.r.o.',
+    latLng: LatLng( 48.98206489218859, 21.24285676870702 ),
+    address: 'Budovateľská 38',
+    zipCode: '080 01',
+    town: 'Prešov',
+    phoneNumber: '0917/213072',
+    email: 'po.ponuky@stavmat.sk',
+    website: 'www.stavmat.sk',
+  ),
+  
+  
+  
+    PaintStore(
+    name: 'Stavebniny DEK s.r.o.',
+    latLng: LatLng( 48.88960043362951, 18.011007968702483 ),
+    address: 'Zlatovská ulica 1921',
+    zipCode: '911 05',
+    town: 'Trenčín',
+    phoneNumber: '02/32 22 30 22',
+    email: 'trencin@dek.sk',
+    website: 'dek.sk',
+  ),
+  
+  
+    
+    PaintStore(
+    name: ' ',
+    latLng: LatLng(  ),
+    address: '',
+    zipCode: '',
+    town: '',
+    phoneNumber: '',
+    email: '',
+    website: '',
+  ),
+  
+    
+  
+    PaintStore(
+    name: 'Dynamik Trading, s.r.o. (člen SOAS)',
+    latLng: LatLng( 48.278011275180724, 18.095145971164055 ),
+    address: 'Novozámocká 104',
+    zipCode: '949 05',
+    town: 'Nitra',
+    phoneNumber: '037/6578205',
+    email: 'stavebniny@dynamik.sk',
+    website: 'www.dynamik.sk',
+  ),
+  
+    
+  
+    PaintStore(
+    name: 'STAVMAT STAVEBNINY s.r.o.',
+    latLng: LatLng( 48.89425061824846, 18.01061598219487 ),
+    address: 'Brnianska 2',
+    zipCode: '911 05',
+    town: 'Trenčín',
+    phoneNumber: '0918/739711',
+    email: 'tn.predajna@stavmat.sk',
+    website: 'www.stavmat.sk',
+  ),
+    
+  
+  
+    PaintStore(
+    name: 'Kartel Nitra s.r.o. (člen ASAS)',
+    latLng: LatLng( 48.30977271501891, 18.07151067052589 ),
+    address: 'Štúrova 155',
+    zipCode: '949 01',
+    town: 'Nitra',
+    phoneNumber: '037/7417071',
+    email: 'kartel@kartel.sk',
+    website: 'www.kartel.sk',
+  ),
+  
+  
+  
+      PaintStore(
+    name: 'Stavebniny DEK s.r.o.',
+    latLng: LatLng( 48.323824746872106, 18.0374620975109 ),
+    address: 'Hlohovecká ulica 4093/8',
+    zipCode: '951 41',
+    town: 'Lužianky',
+    phoneNumber: '02/32 22 30 22',
+    email: 'nitra@dek.sk',
+    website: 'dek.sk',
+  ),
+  
+  
+    
+    PaintStore(
+    name: 'TRIAL s.r.o.',
+    latLng: LatLng( 48.74129917927613, 17.885327968695183 ),
+    address: 'Kočovce 290',
+    zipCode: '916 31',
+    town: 'Kočovce',
+    phoneNumber: '032/7798306',
+    email: 'trial@trialkocovce.sk',
+    website: 'www.trialkocovce.sk',
+  ),
+  
+    
+    PaintStore(
+    name: 'PRESPOR, s.r.o. (člen ASAS)',
+    latLng: LatLng( 47.98303404191944, 18.18427146865854 ),
+    address: 'Viničná 8145/1',
+    zipCode: '940 02',
+    town: 'Nové Zámky',
+    phoneNumber: '035/6423181',
+    email: 'nz@prespor.sk',
+    website: 'www.prespor.sk',
+  ),
+  
+  
+    
+    PaintStore(
+    name: ' ',
+    latLng: LatLng(  ),
+    address: '',
+    zipCode: '',
+    town: '',
+    phoneNumber: '',
+    email: '',
+    website: '',
+  ),
+  
+    
+  
+    PaintStore(
+    name: 'MPL TRADING spol. s r.o.',
+    latLng: LatLng( 47.99950272109134, 18.164417441675006 ),
+    address: 'Výpalisko 12/B',
+    zipCode: '940 01',
+    town: 'Nové Zámky',
+    phoneNumber: '0914/326087',
+    email: 'nz@mpl.sk',
+    website: 'www.mpl.sk',
+  ),
+  
+  
+    
+    PaintStore(
+    name: 'STAVMAT STAVEBNINY, s.r.o.',
+    latLng: LatLng( 49.30403657933367, 21.286997226394654 ),
+    address: 'Duklianska 13',
+    zipCode: '085 01',
+    town: 'Bardejov',
+    phoneNumber: '0917/477226',
+    email: 'bardejov@stavmat.sk',
+    website: 'www.stavmat.sk',
+  ),
+  
+  
+    
+    PaintStore(
+    name: 'Stavebniny DEK s.r.o.',
+    latLng: LatLng( 47.9767358325278, 18.184558595642578 ),
+    address: 'Budovateľská 4',
+    zipCode: '940 53',
+    town: 'Nové Zámky',
+    phoneNumber: '02/32 22 30 22',
+    email: 'nove.zamky@dek.sk',
+    website: 'dek.sk',
+  ),
+  
+  
+    
+    PaintStore(
+    name: 'Hydros Bánovce,spol. s r.o. (člen SOAS)',
+    latLng: LatLng( 48.75976052455747, 17.842916955203975 ),
+    address: 'Trenčianska cesta 14',
+    zipCode: '915 01',
+    town: 'Nové Mesto nad Váhom',
+    phoneNumber: '032/7433110',
+    email: 'stavebninynm@hydros-banovce.sk',
+    website: 'www.hydros-banovce.sk',
+  ),
+  
+    
+  
+    PaintStore(
+    name: 'STAVMAT STAVEBNINY s.r.o.',
+    latLng: LatLng( 47.98978850087319, 18.15800801098697 ),
+    address: 'Bernolákovo námestie 24',
+    zipCode: '940 02',
+    town: 'Nové Zámky',
+    phoneNumber: '0917/127914',
+    email: 'novezamky@stavmat.sk',
+    website: 'www.stavmat.sk',
+  ),
+  
+  
+    
+    PaintStore(
+    name: 'ROPSPOL, a. s.',
+    latLng: LatLng( 48.746690798604774, 17.838704311023616 ),
+    address: 'Trenčianska 18',
+    zipCode: '915 01',
+    town: 'Nové Mesto nad Váhom',
+    phoneNumber: '032/7712680',
+    email: 'predajna@ropspol.sk',
+    website: 'www.ropspol.sk',
+  ),
+  
+  
+  
+    PaintStore(
+    name: 'STAVMAT STAVEBNINY s.r.o.',
+    latLng: LatLng( 48.77002490808598, 17.833474166845054 ),
+    address: 'Srnianska 4',
+    zipCode: '915 01',
+    town: 'Nové Mesto nad Váhom',
+    phoneNumber: '0902/962511',
+    email: 'novemesto@stavmat.sk',
+    website: 'www.stavmat.sk',
+  ),
+  
+  
+    
+    PaintStore(
+    name: 'TONEX PLUS s.r.o. (člen SOAS)',
+    latLng: LatLng( 47.75570017919485, 18.126524226319503 ),
+    address: 'Dunajské nábrežie 1152',
+    zipCode: '945 01',
+    town: 'Komárno',
+    phoneNumber: '035/7731603',
+    email: 'tonex@baucentrum.sk',
+    website: 'www.tonex.sk',
+  ),
+  
+  
+    
+    PaintStore(
+    name: 'STAVMAT STAVEBNINY s.r.o.',
+    latLng: LatLng( 48.373502579242974, 17.618418339841465 ),
+    address: 'Zavarská 10/D',
+    zipCode: '917 01',
+    town: 'Trnava',
+    phoneNumber: '0915/399935',
+    email: 'trnava@stavmat.sk',
+    website: 'www.stavmat.sk',
+  ),
+  
+    
+  
+    PaintStore(
+    name: 'Stavebniny DEK s.r.o.',
+    latLng: LatLng( 48.36566100720531, 17.61351969751297 ),
+    address: 'Ulica Nová 7',
+    zipCode: '917 01',
+    town: 'Trnava',
+    phoneNumber: '02/32 22 30 22',
+    email: 'trnava@dek.sk',
+    website: 'dek.sk',
+  ),
+    
+  
+  
+    PaintStore(
+    name: 'Gabriel Frühvald - STAVEBNINY',
+    latLng: LatLng( 47.967207262765285, 17.786108426329704 ),
+    address: 'Hlavná 138',
+    zipCode: '930 11',
+    town: 'Topoľníky',
+    phoneNumber: '031/5582128',
+    email: 'topolniky@fruhvald.sk',
+    website: 'www.fruhvald.sk',
+  ),
+  
+  
+    
+    PaintStore(
+    name: 'STAVMAT STAVEBNINY, s.r.o.',
+    latLng: LatLng( 48.74693475151187, 21.90798642451578 ),
+    address: 'Zeleninárska 2',
+    zipCode: '071 01',
+    town: 'Michalovce',
+    phoneNumber: '0918/643066',
+    email: 'michalovce@stavmat.sk',
+    website: 'www.stavmat.sk',
+  ),
+  
+  
+    
+    PaintStore(
+    name: 'STAVMAT STAVEBNINY, s.r.o.',
+    latLng: LatLng( 47.987612794933575, 17.628416382150935 ),
+    address: 'Povodská cesta 5',
+    zipCode: '929 01',
+    town: 'Dunajská Streda',
+    phoneNumber: '0918/816378',
+    email: 'ds.predajna@stavmat.sk',
+    website: 'www.stavmat.sk',
+  ),
+  
+  
+    
+    PaintStore(
+    name: 'SUPERCOLORS s.r.o.',
+    latLng: LatLng( 48.14928821370695, 17.517869326338474 ),
+    address: 'Hlavná ulica 1414/138',
+    zipCode: '925 23',
+    town: 'Jelka',
+    phoneNumber: '+421 903 541 082',
+    email: 'supercolors@centrum.sk',
+    website: 'www.supercolors.sk',
+  ),
+  
+  
+    
+    PaintStore(
+    name: 'ONLINESTAVBA s.r.o.',
+    latLng: LatLng( 47.98948995966717, 17.597394897494755 ),
+    address: 'Kračanská cesta 38',
+    zipCode: '929 01',
+    town: 'Dunajská Streda',
+    phoneNumber: '0905574825',
+    email: 'info@onlinestavba.sk',
+    website: 'www.onlinestavba.sk',
+  ),
+  
+  
+    
+    PaintStore(
+    name: 'Stavebniny Skalica s.r.o.',
+    latLng: LatLng( 48.84740947928646, 17.21681969753637 ),
+    address: 'D. Jurkoviča 22',
+    zipCode: '909 01',
+    town: 'Skalica',
+    phoneNumber: '0903/410531',
+    email: 'obchod@stavebninysi.sk',
+    website: 'www.stavebninysi.sk',
+  ),
+  
+  
+    
+    PaintStore(
+    name: 'SATINA, s.r.o.',
+    latLng: LatLng( 48.27996055913423, 17.261209497508823 ),
+    address: 'Bratislavská 79',
+    zipCode: '902 01',
+    town: 'Pezinok',
+    phoneNumber: '033/6400570',
+    email: 'obchod@satina.sk',
+    website: 'www.satina.sk',
+  ),
+  
+    
+  
+    PaintStore(
+    name: 'LTS TRADING, s. r. o.',
+    latLng: LatLng( 48.80252577928208, 17.161487797534164 ),
+    address: 'Petrova Ves 500',
+    zipCode: '908 44',
+    town: 'Petrova Ves',
+    phoneNumber: '0948/777011',
+    email: 'ltstrading@ltstrading.sk',
+    website: 'www.ltstrading.sk',
+  ),
+  
+    
+  
+    PaintStore(
+    name: 'STAVMAT STAVEBNINY, s.r.o.',
+    latLng: LatLng( 48.19013791472021, 17.18985548401232 ),
+    address: 'Tuhovská 1',
+    zipCode: '831 04',
+    town: 'Bratislava-Nové Mesto',
+    phoneNumber: '0903/252984',
+    email: 'ba.predajna@stavmat.sk',
+    website: 'www.stavmat.sk',
+  ),
+  
+    
+  
+    PaintStore(
+    name: 'PRESPOR, s.r.o. (člen ASAS)',
+    latLng: LatLng( 48.173745379226375, 17.155844126339648 ),
+    address: 'Turbínova 1',
+    zipCode: '831 04',
+    town: 'Bratislava-Nové Mesto',
+    phoneNumber: '02/49203251',
+    email: 'prespor@prespor.sk',
+    website: 'www.prespor.sk',
+  ),
+  
+  
+    
+    PaintStore(
+    name: 'OBI Slovakia',
+    latLng: LatLng( 48.14337187922391, 17.150153497502213 ),
+    address: 'Bajkalská 34',
+    zipCode: '821 09',
+    town: 'Bratislava-Ružinov',
+    phoneNumber: '0850/850100',
+    email: 'info@obi.sk',
+    website: 'www.obi.sk',
+  ),
+  
+  
+  
+      PaintStore(
+    name: 'DERMEK Group s.r.o.',
+    latLng: LatLng( 48.65555070466931, 17.016246597527008 ),
+    address: 'Bratislavská 270',
+    zipCode: '908 01',
+    town: 'Kúty',
+    phoneNumber: '0904/360857',
+    email: 'Nebola nájdená ',
+    website: 'www.dermek.group',
+  ),
+  
+  
+    
+    PaintStore(
+    name: 'STAVMAT STAVEBNINY, s.r.o.',
+    latLng: LatLng( 48.432026709959715, 17.02751889936781 ),
+    address: 'Pezinská 56',
+    zipCode: '901 01',
+    town: 'Malacky',
+    phoneNumber: '0903/204543',
+    email: 'ma.predajna@stavmat.sk',
+    website: 'www.stavmat.sk',
+  ),
+  
+  
+    
+    PaintStore(
+    name: 'Stavebniny DEK s.r.o.',
+    latLng: LatLng( 48.25538326600102, 17.03149699750762 ),
+    address: 'Hviezdoslavova 169',
+    zipCode: '900 31',
+    town: 'Stupava',
+    phoneNumber: '02/65935880',
+    email: 'stupava@stavebninydek.sk',
+    website: 'www.dek.sk',
+  ),
+  
+  
+  ];
